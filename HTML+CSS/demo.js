79d702682c6c2e64ed0fe0646506e4649ea0812d
DeepikaBonthu <167194047+DeepikaBonthu@users.noreply.github.com>
2024-04-16 17:10:34 +0530

    Add files via upload

---
+++ HTML+CSS/demo.js
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>demo on java script</title>
+    </head>
+    <body>
+        <script>
+            console.log("hai");
+        </script>
+    </body>
+</html>
